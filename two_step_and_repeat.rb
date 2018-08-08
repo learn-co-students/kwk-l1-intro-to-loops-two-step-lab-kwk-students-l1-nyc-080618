619098badefcc06064a30f9dc0ea17caaf712f69
oliviamaddalone <livmaddalone@gmail.com>
2018-08-08 14:30:26 +0000

    Done.

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,4 +1,16 @@
-def first_steps
+def 
+  number = 1
+  until number = 21
+  puts "Right foot back"
+  sleep (0.5)
+  puts "Left foot back"
+  sleep (0.5)
+  puts "Right foot back"
+  sleep (0.5)
+  puts "Stop"
+  sleep (1)
+end 
+  
   # Write a loop that outputs "Right foot back", "Left foot back",
   # "Right foot back", and "Stop", with sleep after each
 end
